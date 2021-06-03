bda8c6334a0635f8993b4e569de094aa8200aff8
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-03 23:02:19 +0530

    Refactored to return the array instead of printing it

--- LogicalPrograms/src/util/Binary.java
+++ LogicalPrograms/src/util/Binary.java
@@ -5,9 +5,9 @@ public class Binary {
 	/**
 	 * @param decimal number from main method
 	 * Stores binary numbers in an array
-	 * Prints the array
+	 * Returns array
 	 */
-	public static void toBinary(int decimal) {
+	public static int[] toBinary(int decimal) {
 		int bin[]=new int[100];
 		int counter=0;
 		
@@ -16,10 +16,18 @@ public class Binary {
 			bin[counter++]=decimal%2;
 			decimal/=2;
 		}
-		for(int j=counter-1;j>=0;j--)
+		int ret[]=new int[counter];
+		
+		for(int j=counter-1,i=0; j>=0 || i<counter; j--,i++)
 		{
-			System.out.println(bin[j]);
+			ret[i]=bin[j];
 		}
+		for(int i:ret)
+		{
+			System.out.println(i);
+		}
+		System.out.println("///////");
+		return ret;
 	}
 
 }
